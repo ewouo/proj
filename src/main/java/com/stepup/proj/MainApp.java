0818d8d6c306c297ef4a31b56c5f3fcce52e3db8
Elizaveta Melkova <emelkova@inno.tech>
2022-11-24 21:29:38 +0300

    очень важные правки

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,8 +3,10 @@ package com.stepup.proj;
 public class MainApp {
     public static void main (String[] args) {
         System.out.println("Hello world");
-        System.out.println("22222");
+        System.out.println("11122222");
         System.out.println("3");
         int a = 10;
+        System.out.println("1");
+        System.out.println("a=" + a);
     }
 }
