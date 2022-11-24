3079a806685e8edc5216815cbd13a97debc71042
Elizaveta Melkova <emelkova@inno.tech>
2022-11-24 21:27:15 +0300

    добавлена 3

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,7 +3,8 @@ package com.stepup.proj;
 public class MainApp {
     public static void main (String[] args) {
         System.out.println("Hello world");
-        System.out.println("11111");
-        System.out.println("22222   ");
+        System.out.println("22222");
+        System.out.println("3");
+        int a = 10;
     }
 }
