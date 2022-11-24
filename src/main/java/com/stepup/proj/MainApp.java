8b4da57cd56bfbcd37294a5f1c13802288fb8df9
Elizaveta Melkova <emelkova@inno.tech>
2022-11-24 21:22:41 +0300

    выполнены улучшения добавлено 2 строки

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,5 +3,7 @@ package com.stepup.proj;
 public class MainApp {
     public static void main (String[] args) {
         System.out.println("Hello world");
+        System.out.println("11111");
+        System.out.println("22222   ");
     }
 }
